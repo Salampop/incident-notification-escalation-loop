2f72604d72015217004d100d31bae3f3cb8f0ec7
Sam Fawzi <salampop@gmail.com>
2023-04-27 13:21:55 -0400

    initialization

--- functions/helpers/serialize.private.js
+++
@@ -1,8 +0,0 @@
-exports.serialize = function serialize(obj) {
-  const str = [];
-  for (const key in obj)
-    if (obj.hasOwnProperty(key)) {
-      str.push(`${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`);
-    }
-  return str.join('&');
-};
